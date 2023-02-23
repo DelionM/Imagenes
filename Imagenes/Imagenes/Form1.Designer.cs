01d47d4e59f0c1cac7fe96b0512c98f9f82d2c3a
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 22:14:53 -0600

    Add files via upload

---
+++ Imagenes/Imagenes/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Imagenes
+{
+    partial class Estaciones_del_año
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Estaciones_del_año));
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.btnPrimavera = new System.Windows.Forms.Button();
+            this.btnVerano = new System.Windows.Forms.Button();
+            this.btnInvierno = new System.Windows.Forms.Button();
+            this.btnOtoño = new System.Windows.Forms.Button();
+            this.btnMiFoto = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.ptbOtoño = new System.Windows.Forms.PictureBox();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.ptbVerano = new System.Windows.Forms.PictureBox();
+            this.ptbPrimavera = new System.Windows.Forms.PictureBox();
+            this.ptbInvierno = new System.Windows.Forms.PictureBox();
+            this.pcbInicio = new System.Windows.Forms.PictureBox();
+            this.ptbMiFoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbOtoño)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbVerano)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPrimavera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbInvierno)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbInicio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMiFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPrincipal
+            // 
+            this.lblPrincipal.AutoSize = true;
+            this.lblPrincipal.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrincipal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPrincipal.Location = new System.Drawing.Point(427, 9);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(167, 23);
+            this.lblPrincipal.TabIndex = 0;
+            this.lblPrincipal.Text = "Estaciones del año";
+            // 
+            // btnPrimavera
+            // 
+            this.btnPrimavera.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrimavera.Location = new System.Drawing.Point(45, 75);
+            this.btnPrimavera.Name = "btnPrimavera";
+            this.btnPrimavera.Size = new System.Drawing.Size(106, 44);
+            this.btnPrimavera.TabIndex = 1;
+            this.btnPrimavera.Text = "Primavera";
+            this.btnPrimavera.UseVisualStyleBackColor = true;
+            this.btnPrimavera.Click += new System.EventHandler(this.btnPrimavera_Click);
+            // 
+            // btnVerano
+            // 
+            this.btnVerano.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerano.Location = new System.Drawing.Point(45, 125);
+            this.btnVerano.Name = "btnVerano";
+            this.btnVerano.Size = new System.Drawing.Size(106, 44);
+            this.btnVerano.TabIndex = 2;
+            this.btnVerano.Text = "Verano";
+            this.btnVerano.UseVisualStyleBackColor = true;
+            this.btnVerano.Click += new System.EventHandler(this.btnVerano_Click);
+            // 
+            // btnInvierno
+            // 
+            this.btnInvierno.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInvierno.Location = new System.Drawing.Point(45, 175);
+            this.btnInvierno.Name = "btnInvierno";
+            this.btnInvierno.Size = new System.Drawing.Size(106, 44);
+            this.btnInvierno.TabIndex = 3;
+            this.btnInvierno.Text = "Invierno";
+            this.btnInvierno.UseVisualStyleBackColor = true;
+            this.btnInvierno.Click += new System.EventHandler(this.btnInvierno_Click);
+            // 
+            // btnOtoño
+            // 
+            this.btnOtoño.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOtoño.Location = new System.Drawing.Point(45, 225);
+            this.btnOtoño.Name = "btnOtoño";
+            this.btnOtoño.Size = new System.Drawing.Size(106, 44);
+            this.btnOtoño.TabIndex = 4;
+            this.btnOtoño.Text = "Otoño";
+            this.btnOtoño.UseVisualStyleBackColor = true;
+            this.btnOtoño.Click += new System.EventHandler(this.btnOtoño_Click);
+            // 
+            // btnMiFoto
+            // 
+            this.btnMiFoto.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMiFoto.Location = new System.Drawing.Point(45, 295);
+            this.btnMiFoto.Name = "btnMiFoto";
+            this.btnMiFoto.Size = new System.Drawing.Size(106, 44);
+            this.btnMiFoto.TabIndex = 5;
+            this.btnMiFoto.Text = "Mi foto";
+            this.btnMiFoto.UseVisualStyleBackColor = true;
+            this.btnMiFoto.Click += new System.EventHandler(this.btnMiFoto_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(713, 375);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(74, 25);
+            this.btnInicio.TabIndex = 6;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(802, 375);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(78, 25);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnterior.Location = new System.Drawing.Point(172, 185);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(79, 34);
+            this.btnAnterior.TabIndex = 8;
+            this.btnAnterior.Text = "Anterior";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // ptbOtoño
+            // 
+            this.ptbOtoño.Image = global::Imagenes.Properties.Resources.OTOÑO;
+            this.ptbOtoño.Location = new System.Drawing.Point(257, 46);
+            this.ptbOtoño.Name = "ptbOtoño";
+            this.ptbOtoño.Size = new System.Drawing.Size(495, 307);
+            this.ptbOtoño.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbOtoño.TabIndex = 10;
+            this.ptbOtoño.TabStop = false;
+            this.ptbOtoño.Click += new System.EventHandler(this.ptbOtoño_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSiguiente.Location = new System.Drawing.Point(758, 182);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(79, 34);
+            this.btnSiguiente.TabIndex = 11;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // ptbVerano
+            // 
+            this.ptbVerano.Image = global::Imagenes.Properties.Resources.VERANO;
+            this.ptbVerano.Location = new System.Drawing.Point(257, 46);
+            this.ptbVerano.Name = "ptbVerano";
+            this.ptbVerano.Size = new System.Drawing.Size(495, 307);
+            this.ptbVerano.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbVerano.TabIndex = 12;
+            this.ptbVerano.TabStop = false;
+            // 
+            // ptbPrimavera
+            // 
+            this.ptbPrimavera.Image = global::Imagenes.Properties.Resources.primavera_;
+            this.ptbPrimavera.Location = new System.Drawing.Point(257, 46);
+            this.ptbPrimavera.Name = "ptbPrimavera";
+            this.ptbPrimavera.Size = new System.Drawing.Size(495, 307);
+            this.ptbPrimavera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbPrimavera.TabIndex = 13;
+            this.ptbPrimavera.TabStop = false;
+            // 
+            // ptbInvierno
+            // 
+            this.ptbInvierno.Image = global::Imagenes.Properties.Resources.invierno_20181;
+            this.ptbInvierno.Location = new System.Drawing.Point(257, 46);
+            this.ptbInvierno.Name = "ptbInvierno";
+            this.ptbInvierno.Size = new System.Drawing.Size(495, 307);
+            this.ptbInvierno.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbInvierno.TabIndex = 14;
+            this.ptbInvierno.TabStop = false;
+            this.ptbInvierno.Click += new System.EventHandler(this.ptbInvierno_Click);
+            // 
+            // pcbInicio
+            // 
+            this.pcbInicio.Image = global::Imagenes.Properties.Resources.INICIO;
+            this.pcbInicio.Location = new System.Drawing.Point(257, 46);
+            this.pcbInicio.Name = "pcbInicio";
+            this.pcbInicio.Size = new System.Drawing.Size(495, 307);
+            this.pcbInicio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbInicio.TabIndex = 15;
+            this.pcbInicio.TabStop = false;
+            this.pcbInicio.Click += new System.EventHandler(this.pcbInicio_Click);
+            // 
+            // ptbMiFoto
+            // 
+            this.ptbMiFoto.Image = global::Imagenes.Properties.Resources.WhatsApp_Image_2022_03_03_at_22_32_26;
+            this.ptbMiFoto.Location = new System.Drawing.Point(257, 46);
+            this.ptbMiFoto.Name = "ptbMiFoto";
+            this.ptbMiFoto.Size = new System.Drawing.Size(495, 307);
+            this.ptbMiFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbMiFoto.TabIndex = 16;
+            this.ptbMiFoto.TabStop = false;
+            // 
+            // Estaciones_del_año
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(892, 412);
+            this.Controls.Add(this.pcbInicio);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnAnterior);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.btnMiFoto);
+            this.Controls.Add(this.btnOtoño);
+            this.Controls.Add(this.btnInvierno);
+            this.Controls.Add(this.btnVerano);
+            this.Controls.Add(this.btnPrimavera);
+            this.Controls.Add(this.lblPrincipal);
+            this.Controls.Add(this.ptbPrimavera);
+            this.Controls.Add(this.ptbInvierno);
+            this.Controls.Add(this.ptbOtoño);
+            this.Controls.Add(this.ptbMiFoto);
+            this.Controls.Add(this.ptbVerano);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Estaciones_del_año";
+            this.Text = "Estaiones del año";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbOtoño)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbVerano)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPrimavera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbInvierno)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbInicio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMiFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.Button btnPrimavera;
+        private System.Windows.Forms.Button btnVerano;
+        private System.Windows.Forms.Button btnInvierno;
+        private System.Windows.Forms.Button btnOtoño;
+        private System.Windows.Forms.Button btnMiFoto;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.PictureBox ptbOtoño;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.PictureBox ptbVerano;
+        private System.Windows.Forms.PictureBox ptbPrimavera;
+        private System.Windows.Forms.PictureBox ptbInvierno;
+        private System.Windows.Forms.PictureBox pcbInicio;
+        private System.Windows.Forms.PictureBox ptbMiFoto;
+    }
+}
+
---
+++ Imagenes/Imagenes/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imagenes
+{
+    public partial class Estaciones_del_año : Form
+    {
+        public Estaciones_del_año()
+        {
+            InitializeComponent();
+        }
+
+        private void pcbInicio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ptbOtoño_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ptbInvierno_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMiFoto_Click(object sender, EventArgs e)
+        {
+            lblPrincipal.Text = "Mi foto";
+            ptbInvierno.Visible = false;
+            ptbMiFoto.Visible = true;
+            ptbOtoño.Visible = false;
+            pcbInicio.Visible = false;
+            ptbVerano.Visible = false;
+            ptbPrimavera.Visible = false;
+            btnAnterior.Enabled = true;
+            btnSiguiente.Enabled = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea Salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Close();
+                Application.Exit();
+            }
+            else
+            {
+                this.btnInicio.Focus();
+            }
+        }
+
+        public void btnPrimavera_Click(object sender, EventArgs e)
+        {
+
+            lblPrincipal.Text = "Primavera";
+            ptbInvierno.Visible = false;
+            ptbMiFoto.Visible = false;
+            ptbOtoño.Visible = false;
+            pcbInicio.Visible = false;
+            ptbVerano.Visible = false;
+            ptbPrimavera.Visible = true;
+            btnAnterior.Enabled = false;
+            btnSiguiente.Enabled = true;
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            lblPrincipal.Text = "Estaciones del año";
+            ptbInvierno.Visible = false;
+            ptbMiFoto.Visible = false;
+            ptbOtoño.Visible = false;
+            pcbInicio.Visible = true;
+            ptbVerano.Visible = false;
+            ptbPrimavera.Visible = false;
+            btnAnterior.Enabled = false;
+            btnSiguiente.Enabled = true;
+
+        }
+
+        private void btnVerano_Click(object sender, EventArgs e)
+        {
+            lblPrincipal.Text = "Verano";
+            ptbInvierno.Visible = false;
+            ptbMiFoto.Visible = false;
+            ptbOtoño.Visible = false;
+            pcbInicio.Visible = false;
+            ptbVerano.Visible = true;
+            ptbPrimavera.Visible = false;
+            btnAnterior.Enabled = true;
+            btnSiguiente.Enabled = true;
+        }
+
+        private void btnInvierno_Click(object sender, EventArgs e)
+        {
+            lblPrincipal.Text = "Invierno";
+            ptbInvierno.Visible = true;
+            ptbMiFoto.Visible = false;
+            ptbOtoño.Visible = false;
+            pcbInicio.Visible = false;
+            ptbVerano.Visible = false;
+            ptbPrimavera.Visible = false;
+            btnAnterior.Enabled = true;
+            btnSiguiente.Enabled = true;
+        }
+
+        private void btnOtoño_Click(object sender, EventArgs e)
+        {
+            lblPrincipal.Text = "Otoño";
+            ptbInvierno.Visible = false;
+            ptbMiFoto.Visible = false;
+            ptbOtoño.Visible = true;
+            pcbInicio.Visible = false;
+            ptbVerano.Visible = false;
+            ptbPrimavera.Visible = false;
+            btnAnterior.Enabled = true;
+            btnSiguiente.Enabled = true;
+        }
+        
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (lblPrincipal.Text == "Estaciones del año")
+            {
+                lblPrincipal.Text = "Primavera";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = true;
+                btnAnterior.Enabled = false;
+                btnSiguiente.Enabled = true;
+            }
+            else
+                if (lblPrincipal.Text =="Primavera") {
+                lblPrincipal.Text = "Verano";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = true;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = true;
+            }
+            else 
+                if (lblPrincipal.Text== "Verano")
+            {
+                lblPrincipal.Text = "Invierno";
+                ptbInvierno.Visible = true;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = true;
+            }
+            else
+                if (lblPrincipal.Text == "Invierno")
+            {
+                lblPrincipal.Text = "Otoño";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = true;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = true;
+            }
+            else 
+                if (lblPrincipal.Text == "Otoño")
+            {
+                lblPrincipal.Text = "Mi foto";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = true;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = false;
+            }
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (lblPrincipal.Text == "Mi foto")
+            {
+                lblPrincipal.Text = "Otoño";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = true;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = true;
+            }
+            else 
+                if (lblPrincipal.Text == "Otoño")
+            {
+                lblPrincipal.Text = "Invierno";
+                ptbInvierno.Visible = true;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = true;
+            }
+            else
+                if (lblPrincipal.Text == "Invierno")
+            {
+                lblPrincipal.Text = "Verano";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = true;
+                ptbPrimavera.Visible = false;
+                btnAnterior.Enabled = true;
+                btnSiguiente.Enabled = true;
+            }
+            else
+                if (lblPrincipal.Text == "Verano")
+            {
+                lblPrincipal.Text = "Primavera";
+                ptbInvierno.Visible = false;
+                ptbMiFoto.Visible = false;
+                ptbOtoño.Visible = false;
+                pcbInicio.Visible = false;
+                ptbVerano.Visible = false;
+                ptbPrimavera.Visible = true;
+                btnAnterior.Enabled = false;
+                btnSiguiente.Enabled = true;
+            }
+
+        }
+    }
+}
